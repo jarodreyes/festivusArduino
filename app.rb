f8787152fb96968e2425ec7e82d71c895c4af251
jarodreyes <jarodreyes@gmail.com>
2013-12-02 16:16:09 -0800

    respond to random commands.

--- app.rb
+++ app.rb
@@ -13,13 +13,31 @@ before do
   @mmsclient = @client.accounts.get(ENV['TWILIO_SID'])
 end
 
+def sendFestivus(from, to, body, media)
+  if media.nil?
+    message = @client.account.messages.create(
+      :from => from,
+      :to => to,
+      :body => body
+    )
+  else
+    message = @mmsclient.messages.create(
+      :from => from,
+      :to => to,
+      :body => body,
+      :media_url => media,
+    )
+  end
+  puts message.to
+end
+
 get '/live?*' do
   erb :live
 end
 
 get '/trick/?' do
   @phone_number = Sanitize.clean(params[:From])
-  output = "Happy Festivus! The festival for the rest of us!"
+  output = "Happy Festivus from the REST of us!"
   begin
     Pusher['festivus_channel'].trigger('starting:', {:message => 'starting up trick'})
   rescue Pusher::Error => e
@@ -37,10 +55,10 @@ get '/trick/?' do
       puts Pusher['festivus_channel'].trigger('green', {:message => 'green'})
     when 'owl'
       puts Pusher['festivus_channel'].trigger('owl', {:message => 'drawMe'})
-    when 'commands'
-      output = "Available Commands: blue, red, green or ?"
     else
       puts Pusher['festivus_channel'].trigger(command, {:message => command})
+      output = "Available Festivus commands: blue, red, green or ???"
+      skip_media = true
     end
   rescue
     command = "no message"
@@ -50,12 +68,20 @@ get '/trick/?' do
     erb :index, :locals => {:msg => output}
   else
     @picture_url = "http://media.egotvonline.com/wp-content/uploads/2011/11/Funny-Portrait_350x446.jpg"
-    message = @mmsclient.messages.create(
-      :from => 'TWILIO',
-      :to => @phone_number,
-      :body => "Happy Festivus from the REST of us!",
-      :media_url => @picture_url,
-    )
+    if skip_media
+      message = @client.account.messages.create(
+        :from => '2024173378',
+        :to => @phone_number,
+        :body => output
+      )
+    else
+      message = @mmsclient.messages.create(
+        :from => 'TWILIO',
+        :to => @phone_number,
+        :body => output,
+        :media_url => @picture_url,
+      )
+    end
     puts message.to
   end
 end
--- festivus.ino
+++ festivus.ino
@@ -6,6 +6,7 @@
 #define RED    6 
 #define GREEN  9
 #define BLUE   5
+#define FADESPEED 5
 
 byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
 PusherClient client;
@@ -17,9 +18,10 @@ void setup() {
   pinMode(GREEN, OUTPUT);
 
   // We'll default to pink.
-  analogWrite(RED, 80);
+  analogWrite(RED, 0);
   analogWrite(GREEN, 0);
   analogWrite(BLUE, 0);
+  fade();
   
   Serial.begin(9600);
   if (Ethernet.begin(mac) == 0) {
@@ -28,7 +30,7 @@ void setup() {
       ;
   }
   // paste next block of code here.
-  if(client.connect("your-pusher-key")) {
+  if(client.connect("3c75ce0c7941ced00a14")) {
     client.bind("blue", blue);
     client.bind("red", red);
     client.bind("green", green);
@@ -37,6 +39,51 @@ void setup() {
   }
   else {
     while(1) {}
+  }
+}
+
+void fade() {
+  int r, g, b;
+  // fade from white to blue
+  for (b = 0; b < 256; b++) { 
+    analogWrite(BLUE, b);
+    delay(FADESPEED);
+  }
+  // fade from blue to violet
+  for (r = 0; r < 256; r++) { 
+    analogWrite(RED, r);
+    delay(FADESPEED);
+  } 
+  // fade from violet to red
+  for (b = 255; b > 0; b--) { 
+    analogWrite(BLUE, b);
+    delay(FADESPEED);
+  } 
+  // fade from red to yellow
+  for (g = 0; g < 256; g++) { 
+    analogWrite(GREEN, g);
+    delay(FADESPEED);
+  } 
+  // fade from yellow to green
+  for (r = 255; r > 0; r--) { 
+    analogWrite(RED, r);
+    delay(FADESPEED);
+  } 
+  // fade from green to teal
+  for (b = 0; b < 256; b++) { 
+    analogWrite(BLUE, b);
+    delay(FADESPEED);
+  } 
+  // fade from teal to blue
+  for (g = 255; g > 0; g--) { 
+    analogWrite(GREEN, g);
+    delay(FADESPEED);
+  }
+  // fade from blue to white
+  for (b = 0; b < 256; b++) { 
+    analogWrite(RED, b);
+    analogWrite(GREEN, b);
+    delay(FADESPEED);
   }
 }
 
